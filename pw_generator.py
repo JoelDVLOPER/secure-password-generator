9c2518d2b16287482fe00602f8958c8a2c983823
JoelDVLOPER <thekittenkingdom7@gmail.com>
2024-08-31 10:44:20 -0300

    Update pw_generator.py

--- pw_generator.py
+++ pw_generator.py
@@ -1,24 +1,22 @@
 import random
 
 def secure_password():
+    characters = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '123456789', 
+                  "!#$%&/()=?*[]_-.;,:"]
     pass_name = input('Insert your password key name.')
-    characters = [{'alphabet': 'abcdefghijklmnopqrstuvwxyz'},
-                  {'uppercase alpha': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'},
-                  {'digits': '1234567890'},
-                  {'symbols': '!"#$%&/()=?*[]_-.;,:'}]
     pass_len = random.randint(8, 18)
     counter = 0
-    password = []
+    password_chars = []
     while counter < pass_len:
         for c in characters:
-            for g in c.values():
-                h = random.choice(g)
-                password.append(h)
-                counter += 1
-        if counter >= pass_len:
-            break
-    pw = "".join(password)
+            random_character = random.choice(c)
+            password_chars.append(random_character)
+            counter += 1
+            if counter >= pass_len:
+                break
+    pw = "".join(password_chars)
     with open("passwords.txt", "a") as myfile:
         myfile.write(f'{pass_name}: {pw} \n')
+    print('Password(s) saved at passwords.txt.')
 
 secure_password()
